15d1b10c4c53530784fab7ab6bf45c6145b298cd
Paul Campbell <paul@tito.io>
2023-02-08 12:24:17 +0000

    Initial commit of the ga4 plugin

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tito Widget Plugins
+
+This is a repository for plugins for the [Tito V2 Widget](https://ti.to/docs/api/widget/#tito-widget-v2).
+
+## Plugin structure
+
+A plugin is simply code that gets loaded async that uses the Tito widget callbacks
+to run custom code.
---
+++ ga4.js
@@ -0,0 +1,65 @@
+(function () {
+  console.info("[tito][ga4]", "plugin loaded");
+  window.dataLayer = window.dataLayer || [];
+  function ga4() {
+    dataLayer.push(arguments);
+  }
+
+  window.tito =
+    window.tito ||
+    function () {
+      (tito.q = tito.q || []).push(arguments);
+    };
+
+  tito("on:registration:started", function (registration) {
+    console.info("[tito][ga4]", "tracking registration started");
+    const items = [];
+
+    registration.line_items.forEach(function (item) {
+      items.push({
+        item_id: item.id,
+        item_name: item.title,
+        price: item.price,
+        item_category: registration.event.title,
+        item_brand: registration.account.name,
+      });
+    });
+
+    const eventData = {
+      items: items,
+      coupon: registration.discount_code,
+      value: registration.total,
+      currency: registration.currency,
+    };
+
+    ga4("event", "begin_checkout", eventData);
+  });
+
+  tito("on:registration:finished", function (registration) {
+    console.info("[tito][ga4]", "tracking registration finished");
+    const items = [];
+
+    registration.line_items.forEach(function (item) {
+      items.push({
+        item_id: item.id,
+        item_name: item.title,
+        price: item.price,
+        category: registration.event.title,
+        brand: registration.account.name,
+      });
+    });
+
+    const eventData = {
+      transaction_id: "T_12345",
+      tax: registration.tax,
+      coupon: registration.discount_code,
+      transaction_id: registration.slug,
+      affiliation: registration.source,
+      value: registration.total,
+      currency: registration.currency,
+      items: items,
+    };
+
+    ga4("event", "purchase", eventData);
+  });
+})();
